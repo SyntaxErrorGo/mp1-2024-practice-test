75a99b010705c5cc9fc94ac1147eeda1054c4d5f
SyntaxErrorGo <ila.26russ@gmail.com>
2024-12-19 09:27:16 +0300

    Update task1.c

--- 3824Б1ПР3/29 Юнкин Илья Викторович/task1.c
+++ 3824Б1ПР3/29 Юнкин Илья Викторович/task1.c
@@ -1,6 +1,20 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти номер его последнего минимального элемента.
 int task1(int A[], size_t n)
-{
-  return -1;
+{ // в задаче указано что массив не пустой, поэтому без проверки и без return -1;
+    int min_value = A[0];
+    int min_index = 0;
+    for(size_t i = 0; i < n; i++)
+    {
+        if(A[i] < min_value)
+        {
+            min_value = A[i];
+            min_index = i;
+        }
+        else if(A[i] == min_value)
+        {
+            min_index = i; 
+        }
+    }
+    return min_index;
 }
